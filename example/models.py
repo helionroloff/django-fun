0f5b8edeba3586380ece09236d20fb6b81fea855
HelionRoloff <helionroloff@gmail.com>
2021-01-21 14:16:39 -0300

    nova pasta django fun

--- example/models.py
+++ example/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Person(models.Model):
-    SHIRT_SIZES = (
-    ('S', 'Small'),
-    ('M', 'Medium'),
-    ('L', 'Large'),
-    )
     id = models.AutoField(primary_key=True)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=None, blank=True)
@@ -18,9 +18,43 @@ class Musician(Person):
     instrument = models.CharField(max_length=100)
 
 
+
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
 
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+
+
+    def __str__(self):
+        return self.headline
+
+
+    class Meta:
+        ordering = ['headline']
