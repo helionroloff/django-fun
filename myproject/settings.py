d46fcc7998eca7f3287d457950ea154166b25fde
HelionRoloff <helionroloff@gmail.com>
2021-01-25 16:15:08 -0300

    alteração no change password

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'example',
+    'pokemaoc',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'example',
-    'pokemaoc',
-    'users',
+    
    ]
 
 MIDDLEWARE = [
